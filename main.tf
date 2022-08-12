e2461e3c3b917b0cf253dfef52f4fcd643b0336f
devsathyaops <98995934+devsathyaops@users.noreply.github.com>
2022-08-12 15:12:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-resource "aws_spot_instance_request" "spot_req" {
-  ami           = "ami-0d08ef957f0e4722b"
-  spot_price    = "0.03"
-  instance_type = "t2.micro"
-   count = 3
-  tags = {
-    Name = "spot-ins"
-  }
-}
-
-# resource "aws_instance" "ondemand" {
-# ami = "ami-098e42ae54c764c35"
-# instance_type = "t2.micro"
- 
+# resource "aws_spot_instance_request" "spot_req" {
+#   ami           = "ami-0d08ef957f0e4722b"
+#   spot_price    = "0.03"
+#   instance_type = "t2.micro"
+#    count = 3
 #   tags = {
-#     Name = "ONDEMAND"
+#     Name = "spot-ins"
+#   }
 # }
-# }
+
+ resource "aws_instance" "ondemand" {
+ ami = "ami-098e42ae54c764c35"
+ instance_type = "t2.micro"
+ 
+   tags = {
+     Name = "ONDEMAND"
+ }
+ }
