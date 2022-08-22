b3f401a0cc32ce40eb5a37e1bc115ffad1f0f25c
devsathyaops <98995934+devsathyaops@users.noreply.github.com>
2022-08-22 17:20:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "statefile-store-2022-26"
-    key    = "terraform.tfstate"
+    key    = "ec2/bdi_builder/terraform.tfstate"
     region = "us-west-2"
   }
 }
