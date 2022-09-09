be178b9b6761c89784c1178c3cab9754ed2f795f
devsathyaops <98995934+devsathyaops@users.noreply.github.com>
2022-09-09 10:58:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
  resource "aws_spot_instance_request" "spot_req" {
- ami = "ami-0cea098ed2ac54925"
+ #ami = "ami-0cea098ed2ac54925"
     ami           = "ami-0d08ef957f0e4722b"
    spot_price    = "0.03"
    instance_type = "t2.micro"
