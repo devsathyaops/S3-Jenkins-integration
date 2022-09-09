71ce7d2b4a4e3fb6961d00d5e05a061768d5008d
devsathyaops <98995934+devsathyaops@users.noreply.github.com>
2022-09-09 10:39:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "statefile-store-2022-26"
-    key    = "ec2/create_sidekiq_server/i-0675a8a67a0d52444/SPOT/job/"
+    key    = "ec2/create_sidekiq_server/i-0675a8a67a0d52444/SPOT/bdi_builder/terraform.tfstate"
     region = "us-west-2"
   }
 }
